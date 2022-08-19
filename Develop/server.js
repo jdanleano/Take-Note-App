2fd763c5e4d1eb956c345ab703e414e3e87b8edc
Ysfboi <jdanleano@gmail.com>
2022-08-19 00:50:00 -0400

    Attempting to push to heroku

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 // Added a helper to generage unique IDs for the notes
 const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
